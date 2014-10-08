8843c16d9ee8a4a12766a9c0d6dea725ba520178
Kyle Ames <KyleAmes13@gmail.com>
2014-10-07 20:37:02 -0500

    Added up to date json data and scraping scripts

--- scripts/weeklyScores.py
+++ scripts/weeklyScores.py
@@ -4,43 +4,58 @@ weeklyScores.py
 Scrapes the NFL's website to pull the reults from the given year and week's games
 
 @author: ameske
-@date: 8/17/14
+@date: 10/5/14
 '''
+
 from bs4 import BeautifulSoup
 import urllib2
+import json
 
 schedule_url = 'http://www.nfl.com/schedules/2014/REG'
+year = 2014
+week = 5
+
+def home_team(tag):
+    return tag.has_attr('class') and "team-name" in tag['class'] and "home" in tag['class']
+
+def away_team(tag):
+    return tag.has_attr('class') and "team-name" in tag['class'] and "away" in tag['class']
+
+def home_team_score(tag):
+    return tag.has_attr('class') and "team-score" in tag['class'] and "home" in tag['class']
+
+def away_team_score(tag):
+    return tag.has_attr('class') and "team-score" in tag['class'] and "away" in tag['class']
 
 def get_results(year, week_no):
-    games = []
-
     #FETCH THE HTML WITH THE SCORES EMBEDDED
     page = urllib2.urlopen(schedule_url + str(week_no))
     resultsHTML = BeautifulSoup(page.read())
     
     #WE CAN UNIQUELY ID A GAME BASED ON THE HOME TEAM, GATHER THEM AND THE SCORES
-    home_teams          = [ str(tag.string) for tag in resultsHTML.find_all(class_="team-name home ") ]
-    home_team_scores    = [ str(tag.string) for tag in resultsHTML.find_all(class_="team-score home ") ]
-    away_team_scores    = [ str(tag.string) for tag in resultsHTML.find_all(class_="team-score away ") ]
-    
+    home_teams          = [ str(tag.string) for tag in resultsHTML.find_all(home_team) ]
+    home_team_scores    = [ str(tag.string) for tag in resultsHTML.find_all(home_team_score) ]
+    away_team_scores    = [ str(tag.string) for tag in resultsHTML.find_all(away_team_score) ]
     results = zip(home_teams, home_team_scores, away_team_scores)
     
+    #JSON'IFY THE RESULTS
+    games = []
     for result in results:
         d = {
                 "year": year,
                 "week": week_no,
                 "home": result[0],
                 "home_score": result[1],
-                "away_score": result[0],
+                "away_score": result[2],
         }                
         games.append(d)
                 
     return games
 
 def main():
-  results = get_results(int(year), int(week)))
-  fd = open(year + '-Week' + week + '_Results.json')
-  fd.write(json.dumps(results))
+  results = get_results(int(year), int(week))
+  fd = open('{0}-Week{1}-Results.json'.format(year, week), 'w')
+  fd.write(json.dumps(results, indent=4, separators=(',', ': ')))
   fd.close()
 
 if __name__ == "__main__":
