a78106882098f32b9bb7511686c1188107f3c646
Kyle Ames <KyleAmes13@gmail.com>
2014-10-22 20:53:46 -0500

    Stubbed out the picks endpoint

--- picks.go
+++ picks.go
@@ -1,7 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/gorilla/context"
+)
+
+// Picks fetches this week's picks for the current logged in user and renders the
+// picks template with them.
 func Picks(w http.ResponseWriter, r *http.Request) {
+	user := context.Get(r, "user").(string)
 
+	// STUB
+	w.Write([]byte(user))
 }
